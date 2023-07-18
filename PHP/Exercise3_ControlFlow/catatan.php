8eac54ad3b92c5c97c60747d57b01e30dd6fb704
FeryEka <Ferrh.mahendra@gmail.com>
2023-07-18 15:39:53 +0700

    add catatan php tentang control flow

---
+++ PHP/Exercise3_ControlFlow/catatan.php
@@ -0,0 +1,26 @@
+<?php
+    // PENGULANGAN
+    // for
+    // while
+    // do.. while
+    // foreach | (note: pengulangan khusus array)
+    
+    // -FOR-
+    for ($i=1; $i < 5; $i++) { 
+        echo $i . "Hello World! <br>";
+    }
+
+    // -WHILE-
+    $a = 0;
+    while ($a <= 10) {
+        echo "Hellow World! <br>";
+        $a++;
+    }
+
+    // -DO.. WHILE-
+    $x = 0;
+    do {
+        echo "Hello World? <br>";
+        $x++;
+    } while($x < 5);
+?>
