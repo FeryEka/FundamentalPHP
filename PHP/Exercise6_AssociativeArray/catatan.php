58428e600bf1a1e52d75326873fd142b7538a762
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-02 14:53:47 +0700

    new latihan 6 assosiative array. file review materi array

---
+++ PHP/Exercise6_AssociativeArray/catatan.php
@@ -0,0 +1,21 @@
+<?php
+    // Array
+    // membuat array
+
+    // membuat array
+    // cara lama
+    $hari = array("senin", "selasa", "rabu");
+    // cara baru
+    $bulan = ["Januari", "Februari", "Maret"];
+    $arr1 = [100, "Halo", true];
+
+    // menampilkan array
+    // versi debugging
+    var_dump($hari);
+    echo "<br>";
+    print_r($bulan);
+    echo "<br>";
+
+    // menampilkan 1 elemen pada array
+    echo $arr1[0];
+?>
