6469531281a47852fce31ea63719aa229b584952
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-03 15:44:08 +0700

    new materi POST

---
+++ PHP/Exercise7_GETandPOST/exercise3_POST.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST</title>
+</head>
+<body>
+    <form action="exercise4_POST" method="post">
+        <p>masukkan nama :</p>
+        <input type="text" name="nama" required>
+        <br>
+        <button type="submit" name="submit" id="nama">kirim</button>
+    </form>
+</body>
+</html>
