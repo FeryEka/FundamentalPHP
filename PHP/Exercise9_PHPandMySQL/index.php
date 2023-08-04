1cca01470abcbff4cedb720a33e81878153fcfc1
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-04 17:09:43 +0700

    new materi php and mysql db

---
+++ PHP/Exercise9_PHPandMySQL/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Smartphones</h1>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>Merek</th>
+            <th>Chipset</th>
+            <th>Ram</th>
+            <th>Harga</th>
+            <th>Gambar</th>
+            <th>Aksi</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>aaaa</td>
+            <td>aaaa</td>
+            <td>aaaa</td>
+            <td>aaaa</td>
+            <td>aaaa</td>
+            <td><img src="" alt=""></td>
+            <td>
+                <a href="">Edit</a> |
+                <a href="">Delete</a>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
