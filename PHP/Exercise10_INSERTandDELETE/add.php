eab48e2c06cf8cea8a4d6baf0a140865e2b69152
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-08 22:03:44 +0700

    add php ditambahkan

---
+++ PHP/Exercise10_INSERTandDELETE/add.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Smartphone</title>
+</head>
+<body>
+    <h1>Tambah Data Smartphone</h1>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="merek">Merek : </label>
+                <input type="text" name="merek" id="merek">
+            </li>
+            <li>
+                <label for="chipset">Chipset : </label>
+                <input type="text" name="chipset" id="chipset">
+            </li>
+            <li>
+                <label for="ram">RAM : </label>
+                <input type="text" name="ram" id="ram">
+            </li>
+            <li>
+                <label for="harga">Harga : </label>
+                <input type="text" name="harga" id="harga">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Add Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
--- PHP/Exercise10_INSERTandDELETE/index.php
+++ PHP/Exercise10_INSERTandDELETE/index.php
@@ -14,6 +14,8 @@
 <body>
     <h1>Daftar Smartphones</h1>
 
+    <a href="tambah.php">Tambah data list smartphone</a>
+    <br><br>
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>No.</th>
