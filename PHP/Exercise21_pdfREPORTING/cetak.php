94ca9dc1b53e526fbe0e596b6afa1709f07366de
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-22 03:30:13 +0700

    add library mpdf using composer

--- PHP/Exercise21_pdfREPORTING/cetak.php
+++ PHP/Exercise21_pdfREPORTING/cetak.php
@@ -1,6 +1,7 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
+require 'connect.php';
+    $smartphones = query("SELECT * FROM smartphones");
 
 $mpdf = new \Mpdf\Mpdf();
 $mpdf->WriteHTML('<h1>Hello world!</h1>');
