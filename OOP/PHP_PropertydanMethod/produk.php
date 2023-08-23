a9bb46a209fb766e7a5b8431fd83a18bcd326aeb
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-24 00:20:41 +0700

    add function kalau di dalam class namanya method

--- OOP/PHP_PropertydanMethod/produk.php
+++ OOP/PHP_PropertydanMethod/produk.php
@@ -9,6 +9,11 @@
                $penulis = "penulisan" ,
                $penerbit = "penerbitan",
                $harga = 0;
+
+        // membuat method
+        public function sayHello() {
+            return "Hello Wordl!";
+        }
     }
 
     // ini merupakan objek produk1
@@ -20,4 +25,13 @@
     $produk2 = new Produk();
     $produk2->judul = "Naruto Shippuden";
     var_dump($produk2);
+
+    $produk3 = new Produk();
+    $produk3->judul = "Naruto";
+    $produk3->penulis = "Masashi Kishimoto";
+    $produk3->penerbit = "Shonen Jump";
+    $produk3->harga = 30000;
+
+    echo "Komik: $produk3->penulis, $produk3->3"
+
 ?>
