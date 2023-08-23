7795813d46f60d10a4ca553114c9546b512c0451
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-23 23:43:06 +0700

    new belajar oop PHP property dan method

---
+++ OOP/PHP_PropertydanMethod/produk.php
@@ -0,0 +1,15 @@
+<?php
+    // Jualan Produk
+    // Komik
+    // Game
+
+    class Produk {
+        public $judul, 
+               $penulis,
+               $penerbit,
+               $harga;
+    }
+
+    $produk1 = new Produk();
+    $produk2 = new Produk();
+?>
