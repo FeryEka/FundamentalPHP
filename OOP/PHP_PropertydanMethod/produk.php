3e6b114be10b41f775979333068e9f24953074d5
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-23 23:50:55 +0700

    penerapannya

--- OOP/PHP_PropertydanMethod/produk.php
+++ OOP/PHP_PropertydanMethod/produk.php
@@ -4,12 +4,20 @@
     // Game
 
     class Produk {
-        public $judul, 
-               $penulis,
-               $penerbit,
-               $harga;
+        // membuat property dan dapat diisi nilainya sebagai default
+        public $judul = "judul", 
+               $penulis = "penulisan" ,
+               $penerbit = "penerbitan",
+               $harga = 0;
     }
 
+    // ini merupakan objek produk1
     $produk1 = new Produk();
+    // menimpa nilai property default pada class produk
+    $produk1->judul = "Naruto";
+    var_dump($produk1);
+
     $produk2 = new Produk();
+    $produk2->judul = "Naruto Shippuden";
+    var_dump($produk2);
 ?>
