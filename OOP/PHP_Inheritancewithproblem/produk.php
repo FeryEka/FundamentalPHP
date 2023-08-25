619120e4339011b425e26cac1a84541b82d08e84
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-25 23:58:46 +0700

    perbedaan inheritance

--- OOP/PHP_Inheritancewithproblem/produk.php
+++ OOP/PHP_Inheritancewithproblem/produk.php
@@ -10,14 +10,20 @@ use Produk as GlobalProduk;
         public $judul, 
                $penulis,
                $penerbit,
-               $harga;
+               $harga,
+               $jmlHalaman,
+               $waktuMain,
+               $tipe;
 
         // Membuat Construct
-        public function __construct($judul = "judul", $penulis = "penulisan", $penerbit = "penerbitan", $harga = 0){
+        public function __construct($judul = "judul", $penulis = "penulisan", $penerbit = "penerbitan", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe ){
             $this->judul = $judul;
             $this->penulis = $penulis;
             $this->penerbit = $penerbit;
             $this->harga = $harga;
+            $this->jmlHalaman = $jmlHalaman;
+            $this->waktuMain = $waktuMain;
+            $this->tipe = $tipe;
         }
 
         // membuat method (sebenernya function kalo di dalam class penyebutannya method)
@@ -28,21 +34,23 @@ use Produk as GlobalProduk;
     }
 
     class CetakInfoProduk {
-        public function cetak( Produk $produk ) {
-            $str = "{$produk->judul} | {$produk->getLabel()} (Rp. $produk->harga)";
-            return $str;
-        }
+        //  Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+        $str = "{$this->tipe}:{$this->judul}|{$this->getLabel()} (Rp. {$this->harga})";
     }
 
     // Object
-    $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
-    $produk2 = new Produk("Genshin Impact", "Dawei", "Hoyoverse", 0);
+    $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+    $produk2 = new Produk("Genshin Impact", "Dawei", "Hoyoverse", 0, 0, 50, "Game");
 
-    echo "Komik : " . $produk1->getLabel();
-    echo "<br>";
-    echo "Game : " . $produk2->getLabel();
-    echo "<br>";
+    // echo "Komik : " . $produk1->getLabel();
+    // echo "<br>";
+    // echo "Game : " . $produk2->getLabel();
+    // echo "<br>";
 
-    $infoProduk1 = new CetakInfoProduk();
-    echo $infoProduk1->cetak($produk1);
+    // $infoProduk1 = new CetakInfoProduk();
+    // echo $infoProduk1->cetak($produk1);
+
+    //  Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+    //  Game : Genshin | Dawei, Hoyoverse (Rp. 0) - Ongoing
+
 ?>
