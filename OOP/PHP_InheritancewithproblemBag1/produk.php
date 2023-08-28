4f837929ed353ed9f1037f104a2762c6fb594177
FeryEka <Ferrh.mahendra@gmail.com>
2023-08-28 15:18:42 +0700

    rename bag1 dan add new belajar inheritance bag2

---
+++ OOP/PHP_InheritancewithproblemBag1/produk.php
@@ -0,0 +1,62 @@
+<?php
+    // Jualan Produk
+    // Komik
+    // Game
+
+use Produk as GlobalProduk;
+
+    class Produk {
+        // membuat property dan dapat diisi nilainya sebagai default
+        public $judul, 
+               $penulis,
+               $penerbit,
+               $harga,
+               $jmlHalaman,
+               $waktuMain,
+               $tipe;
+
+        // Membuat Construct
+        public function __construct($judul = "judul", $penulis = "penulisan", $penerbit = "penerbitan", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe ){
+            $this->judul = $judul;
+            $this->penulis = $penulis;
+            $this->penerbit = $penerbit;
+            $this->harga = $harga;
+            $this->jmlHalaman = $jmlHalaman;
+            $this->waktuMain = $waktuMain;
+            $this->tipe = $tipe;
+        }
+
+        // membuat method (sebenernya function kalo di dalam class penyebutannya method)
+        public function getLabel() {
+            // $this-> akan mengacu pada variable pada property class
+            return "$this->penulis, $this->penerbit";
+        }
+
+        public function getInfoLengkap() {
+            //  Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+            $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+            if( $this->tipe == "Komik"){
+                $str .= " - {$this->jmlHalaman} Halaman.";
+            } else if( $this->tipe == "Game"){
+                $str .= " ~ {$this->waktuMain} Jam.";
+            }
+            return $str;
+        }
+    }
+
+    class CetakInfoProduk {
+        public function cetak( Produk $produk ) {
+            $str = "{$produk->judul} | {$produk->getLabel()} (Rp. $produk->harga)";
+            return $str;
+        }
+    }
+
+    // Object
+    $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+    $produk2 = new Produk("Genshin Impact", "Dawei", "Hoyoverse", 0, 0, 50, "Game");
+
+    echo $produk1->getInfoLengkap();
+    echo "<br>";
+    echo $produk2->getInfoLengkap();
+
+?>
