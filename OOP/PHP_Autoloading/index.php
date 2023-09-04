158d09118b8aaf6c8dd5e0c94502ce0ae5e81d7c
FeryEka <Ferrh.mahendra@gmail.com>
2023-09-04 20:27:47 +0700

    beberapa perbaikan dan cara menyambungkan antara file class

---
+++ OOP/PHP_Autoloading/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+    $produk2 = new Game("Genshin Impact", "Dawei", "Hoyoverse", 0, 50);
+    $produk3 = new Game("Resident Evil 4 Remake", "Shinji Mikami dan Haruo Murata", "Capcom", 831000, 24);
+
+    $cetakProduk = new CetakInfoProduk();
+    $cetakProduk->tambahProduk( $produk1 );
+    $cetakProduk->tambahProduk( $produk2 );
+    $cetakProduk->tambahProduk( $produk3 );
+    echo $cetakProduk->cetak();
+?>
