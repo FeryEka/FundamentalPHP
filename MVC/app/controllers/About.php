e84b4851d8de31f156d7f30a522c3bb405605b2f
FeryEka <Ferrh.mahendra@gmail.com>
2023-09-07 12:29:23 +0700

    new belajar penerapan MVC bagian view
    dan fix routing error ketika akses dengan url MVC/public/

--- MVC/app/controllers/About.php
+++ MVC/app/controllers/About.php
@@ -1,11 +1,19 @@
 <?php
-    class About{
+    class About extends controller{
         public function index($nama = 'Fery Eka Mahendra', $jurusan = 'Teknik Informatika'){
-            echo "Halo, nama saya $nama, saya dari jurusan $jurusan";
+            $data['nama'] = $nama;
+            $data['jurusan'] = $jurusan;
+            $data['judul'] = 'About Me';
+            $this->view('templates/header', $data);
+            $this->view('about/index', $data);
+            $this->view('templates/footer');
         }
 
         public function page(){
-            echo 'About/page';
+            $data['judul'] = 'Pages';
+            $this->view('templates/header', $data);
+            $this->view('about/page');
+            $this->view('templates/footer');
         }
     }
 ?>
