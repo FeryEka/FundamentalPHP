e03666c01b1ca35ba101e6e36df2ed693c130f02
FeryEka <Ferrh.mahendra@gmail.com>
2023-09-10 19:49:50 +0700

    new belajar peneparan MVC flashmassage

---
+++ MVC/mvc_flashmessage/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('BASEURL', 'http://localhost:8080/FundamentalPHP/MVC/mvc_flashmessage/public');    
+
+    // database
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'rykara_cell');
---
+++ MVC/mvc_flashmessage/app/models/Smartphone_model.php
@@ -0,0 +1,40 @@
+<?php
+    class Smartphone_model{
+        private $table = 'smartphones';
+        private $db;
+
+        public function __construct()
+        {
+            $this->db = new Database;
+        }
+
+        public function getAllSmartphone(){
+            $this->db->query('SELECT * FROM '. $this->table);
+            return $this->db->resultSet();
+
+        }
+
+        public function getSmartphoneById($id){
+            $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+            $this->db->bind('id', $id);
+            return $this->db->single();
+        }
+
+        public function tambahDataSmartphone($data){
+            $query = "INSERT INTO smartphones 
+                        VALUES
+                        ('', :nama, :merek, :chipset, :ram, :harga, :gambar)";
+
+            $this->db->query($query);
+            $this->db->bind('nama', $data['nama']);
+            $this->db->bind('merek', $data['merek']);
+            $this->db->bind('chipset', $data['chipset']);
+            $this->db->bind('ram', $data['ram']);
+            $this->db->bind('harga', $data['harga']);
+            $this->db->bind('gambar', $data['gambar']);
+
+            $this->db->execute();
+
+            return $this->db->rowCount();
+        }
+    }
