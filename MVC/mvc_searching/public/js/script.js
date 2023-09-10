3e50d9ca4f706f3e4b36e79698d3de58e9e57bcd
FeryEka <Ferrh.mahendra@gmail.com>
2023-09-11 03:55:25 +0700

    new penerapan fungsi searching pada MVC

---
+++ MVC/mvc_searching/public/js/script.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('.tombolTambahData').on('click', function() {
+        $('#formModalLabel').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+    });
+
+    $('.tampilModalUbah').on('click', function() {
+        $('#formModalLabel').html('Ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal-body form').attr('http://localhost:8080/FundamentalPHP/MVC/mvc_searching/public/smartphone/ubah');
+
+        const id = $(this).data('id');
+        
+        $.ajax({
+            url: 'http://localhost:8080/FundamentalPHP/MVC/mvc_searching/public/smartphone/getubah',
+            data: {id : id},
+            method: 'post',
+            dataType: 'json',
+            success: function(data) {
+                $('#id').val(data.id);
+                $('#nama').val(data.nama);
+                $('#merek').val(data.merek);
+                $('#chipset').val(data.chipset);
+                $('#ram').val(data.ram);
+                $('#harga').val(data.harga);
+                $('#gambar').val(data.gambar);
+            }
+        });
+    });
+});
